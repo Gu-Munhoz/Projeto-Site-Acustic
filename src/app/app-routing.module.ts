be23d1e33345794159ceee2528ae0fc2f6a6b1c6
Gu-Munhoz <gumunhoz30@gmail.com>
2022-03-07 23:17:21 -0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContatoComponent } from './contato/contato.component';
+import { EmpresaComponent } from './empresa/empresa.component';
+import { HomeComponent } from './home/home.component';
+import { ProdutosComponent } from './produtos/produtos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'empresa', component: EmpresaComponent},
+  {path: 'produtos', component: ProdutosComponent},
+  {path: 'contato', component: ContatoComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { EmpresaComponent } from './empresa/empresa.component';
+import { ProdutosComponent } from './produtos/produtos.component';
+import { ContatoComponent } from './contato/contato.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    EmpresaComponent,
+    ProdutosComponent,
+    ContatoComponent
   ],
   imports: [
     BrowserModule,
